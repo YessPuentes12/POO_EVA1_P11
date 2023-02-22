6d5003643be3fac2aaa6915fceb2ffa2265fcfe2
Yessly Mayorga <106281825+YessPuentes12@users.noreply.github.com>
2023-02-22 09:45:45 -0600

    Add files via upload

---
+++ EVA1_11_RETURN/src/eva1_11_return/EVA1_11_RETURN.java
@@ -0,0 +1,20 @@
+/*
+
+ */
+package eva1_11_return;
+
+
+
+public class EVA1_11_RETURN {
+
+  
+    public static void main(String[] args) {
+     int R;
+    R = square(10);//llamada a función
+    System.out.println("Resultado: "+R);
+    }
+    
+   public static int square (int num){
+    return num*num;
+   } 
+}
